e51644e68408474e5c0a49f4ed7b4febee400654
Dmitry Ponomarev <demdxx@trafficstars.com>
2020-07-16 13:51:32 +0200

    Add event publisher wrapper

--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.14
 
 require (
 	github.com/demdxx/rpool v0.0.0-20200317152850-d737c64d8aaf
-	github.com/geniusrabbit/notificationcenter v1.0.0
+	github.com/geniusrabbit/notificationcenter v1.3.1
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.5.1
-	github.com/theplant/testingutils v0.0.0-20190603093022-26d8b4d95c61
+	github.com/stretchr/testify v1.6.1
 )
--- publisher.go
+++ publisher.go
@@ -15,3 +15,24 @@ func Retranslator(pub Publisher) Task {
 		return pub.Publish(ctx, event)
 	})
 }
+
+type publisherEventWrapper struct {
+	name string
+	pub  Publisher
+}
+
+// PublisherEventWrapper with fixed event name
+func PublisherEventWrapper(eventName string, publisher Publisher) Publisher {
+	return &publisherEventWrapper{
+		name: eventName,
+		pub:  publisher,
+	}
+}
+
+func (wr *publisherEventWrapper) Publish(ctx context.Context, messages ...interface{}) error {
+	events := make([]interface{}, 0, len(messages))
+	for _, msg := range messages {
+		events = append(events, WithPayload(wr.name, msg))
+	}
+	return wr.pub.Publish(ctx, events...)
+}
